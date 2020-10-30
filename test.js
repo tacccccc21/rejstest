0b3af43c67f580e7ae058e9aa043826c2b3be494
tac <tacccccc21@gmail.com>
2020-10-30 17:03:01 +0900

    five

---
+++ test.js
@@ -0,0 +1,15 @@
+let gt ="fall";
+
+  // gt = "up";
+
+
+  // 配列
+let five = ['いちか','にの','みく','よつば','いつき'];
+
+// ループ文
+let index = 0;
+while(index < five.length){
+  // 繰り返し文
+  console.log(five[index])
+  index++;
+}
