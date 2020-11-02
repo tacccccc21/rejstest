15a3ba724e965704c197ccca4d13a276b7e55127
tac <tacccccc21@gmail.com>
2020-11-02 16:13:41 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,12 +7,10 @@ let gt ="fall";
 let five = ['いちか','にの','みく','よつば','いつき'];
 
 // ループ文
-// let index = 0;
-// while(index < five.length){
-//   // 繰り返し文
-//   index++;
-// }
-
-if(){
-  
+let index = 0;
+while(index < five.length){
+  // 繰り返し文
+  console.log(five[index]);
+  index++;
 }
+
