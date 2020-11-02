debff0c2943c839c9c6e80e4e3665b571ea2b64c
tac <tacccccc21@gmail.com>
2020-11-02 16:57:13 +0900

    add

--- test.js
+++ test.js
@@ -7,10 +7,60 @@ let gt ="fall";
 let five = ['いちか','にの','みく','よつば','いつき'];
 
 // ループ文
-let index = 0;
-while(index < five.length){
-  // 繰り返し文
-  console.log(five[index]);
-  index++;
-}
+// let index = 0;
+// while(index < five.length){
+//   // 繰り返し文
+//   console.log(five[index]);[]
+//   index++;
+// }
 
+// if else 
+// if(five.length > 5){
+//   console.log('中野姉妹');
+// } else {
+// console.log('aaaa');
+// }
+
+
+// 関数
+const sister = (arg) => {
+  //  ここに命令をかく
+  if(five.length === arg){
+    console.log('中野姉妹');
+  } else {
+  console.log('五つ子じゃないです');
+  }
+};
+
+sister(5);
+
+const ichika = {
+  size: 'big',
+  color: 'pink',
+  number: '1',
+  uesugiCall: () => {
+    console.log('上杉くん')
+  }
+};
+
+
+
+document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
+  window.alert('長女')
+});
+
+document.getElementsByTagName('button')[1].addEventListener('click', ()=> {
+  window.alert('二女')
+});
+
+document.getElementsByTagName('button')[2].addEventListener('click', ()=> {
+  window.alert('三女')
+});
+
+document.getElementsByTagName('button')[3].addEventListener('click', ()=> {
+  window.alert('四女')
+});
+
+document.getElementsByTagName('button')[4].addEventListener('click', ()=> {
+  window.alert('五女')
+});
